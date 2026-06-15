22199eb81d515ecca48fc748e90a088a616fdc98
mahmoud kolib <mahmoud.kolib22@gmail.com>
2026-06-16 02:50:49 +0300

    chore: update runtime identifier and NuGet tool version across projects

--- src/PaymentService.Domain/obj/Debug/net9.0/PaymentService.Domain.AssemblyInfo.cs
+++ src/PaymentService.Domain/obj/Debug/net9.0/PaymentService.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentService.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3346f98b9e59eef94c3880404d9f2d1ccc84d305")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+274a0b1145ec3f5e358500860fb0d3bd8335be69")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentService.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentService.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
